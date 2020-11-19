45e43f9955005cca2b781115a1c5fe99a751fd88
Macintosh HD <macintoshhd@Macintoshs-iMac.local>
2020-11-19 16:23:16 +0400

    feat(5.Arrays):HW-2

---
+++ 5.Arrays/Homework/task-2.js
@@ -0,0 +1,32 @@
+Array.prototype.myReverse = function() {
+    let arrLength = this.length;
+    for(let i=0;i<Math.floor(arrLength/2);++i){
+        let temp = this[i];
+        this[i] = this[arrLength - i - 1];
+        this[arrLength - i - 1] = temp;
+        return this;
+    }
+}
+function reverse(arr){
+    try{
+        if(!Array.isArray(arr)){
+            throw Error("Parameter passed must be an array!");
+        }else if(arr.length === 0){
+            throw Error("Your array mustn't be empty");
+        }
+    }catch(err){
+        console.log(err.message);
+    }
+    // let arrLength = arr.length;
+    // for(let i=0;i<Math.floor(arrLength/2);++i){
+    //     let temp = arr[i];
+    //     arr[i] = arr[arrLength - i - 1];
+    //     arr[arrLength - i - 1] = temp;
+    //     console.log(arr)
+    // }
+    return arr.myReverse();
+}
+
+
+
+console.log(reverse([1,2,3]));
