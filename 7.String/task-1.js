824bd64fd8cf3c99766f70e7bc5a8122e35bbb33
Macintosh HD <macintoshhd@Macintoshs-iMac.local>
2020-11-24 13:56:18 +0400

    feat(7.String):HW1 add throw

--- 7.String/task-1.js
+++ 7.String/task-1.js
@@ -1,4 +1,7 @@
-let rmHtmlTags = (str="")=>{
+let rmHtmlTags = (str)=>{
+    if(typeof str !== "string"){
+        throw new Error("Not a String");
+    }
 
     str = str.replace(/<\/?\w+\/?>/gi,"");
     console.log(str);
@@ -6,3 +9,4 @@ let rmHtmlTags = (str="")=>{
 
 rmHtmlTags('<p><strong><em>Content</em></strong></p>')
 rmHtmlTags('<p><strong><em>Cont</br>ent2</br></em></strong></p>')
+rmHtmlTags([])
